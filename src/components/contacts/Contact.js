956ac4d4389884fc4d9b3f3e08cdb98792a6ac8b
Brandon A <jbrandonarnold@gmail.com>
2020-09-12 13:36:28 -0400

    added delete function

--- src/components/contacts/Contact.js
+++ src/components/contacts/Contact.js
@@ -6,31 +6,37 @@ class Contact extends Component {
         showContactInfo: true,
     }
 
-    onShowClick = e => {
-        this.setState(
-            { showContactInfo: !this.state.showContactInfo }
-        )
+    onDeleteClick = () => {
+        this.props.deleteClickHandler()
     }
 
     render() {
-        const { name, email, phone } = this.props
+        const { name, email, phone } = this.props.contact;
+        const { showContactInfo } = this.state;
         return (
             <div className="card card-body mb-3">
-                <h4>{name}{' '} <i
-                    onClick={this.onShowClick}
-                    className="fas fa-sort-down" /></h4>
-                <ul className="list-group">
+                <h4>{name}{' '}
+                    <i
+                        onClick={() => this.setState({ showContactInfo: !this.state.showContactInfo })}
+                        className="fas fa-sort-down"
+                        style={{ cursor: 'pointer' }} />
+                    <i
+                        className="fas fa-times"
+                        style={{ cursor: 'pointer', float: 'right', color: 'red' }}
+                        onClick={this.onDeleteClick} />
+                </h4>
+                {showContactInfo ? (<ul className="list-group">
                     <li className="list-group-item">{email}</li>
                     <li className="list-group-item">{phone}</li>
-                </ul>
+                </ul>) : null}
+
             </div>
         )
     }
 }
 
 Contact.propTypes = {
-    name: PropTypes.string.isRequired,
-    email: PropTypes.string.isRequired,
-    phone: PropTypes.string.isRequired
+    contact: PropTypes.object.isRequired,
+    deleteClickHandler: PropTypes.func.isRequired
 }
 export default Contact;
--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -2,38 +2,47 @@ import React, { Component } from 'react';
 import Contact from './Contact';
 
 class Contacts extends Component {
-    constructor() {
-        super();
-        this.state = {
-            contacts: [
-                {
-                    id: 1,
-                    name: 'John Doe',
-                    email: 'JDoe@gmail.com',
-                    phone: '444-444-4444'
-                },
-                {
-                    id: 2,
-                    name: 'Henry Doe',
-                    email: 'HDoe@gmail.com',
-                    phone: '555-555-5555'
-                },
-                {
-                    id: 3,
-                    name: 'Mary Doe',
-                    email: 'MDoe@gmail.com',
-                    phone: '666-666-6666'
-                },
+    state = {
+        contacts: [
+            {
+                id: 1,
+                name: 'John Doe',
+                email: 'JDoe@gmail.com',
+                phone: '444-444-4444'
+            },
+            {
+                id: 2,
+                name: 'Henry Doe',
+                email: 'HDoe@gmail.com',
+                phone: '555-555-5555'
+            },
+            {
+                id: 3,
+                name: 'Mary Doe',
+                email: 'MDoe@gmail.com',
+                phone: '666-666-6666'
+            },
 
-            ]
-        }
+        ]
     }
+
+    deleteContact = (id) => {
+        const { contacts } = this.state;
+
+        const newContacts = contacts.filter(contact => contact.id !== id)
+
+        this.setState({
+            contacts: newContacts
+        })
+    }
+
     render() {
         const { contacts } = this.state;
         return (
             <React.Fragment>
                 {contacts.map(contact => (
-                    <Contact name={contact.name} email={contact.email} phone={contact.phone} />
+                    <Contact key={contact.id} contact={contact}
+                        deleteClickHandler={this.deleteContact.bind(this, contact.id)} />
                 ))}
             </React.Fragment>
         )
